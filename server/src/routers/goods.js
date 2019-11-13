53c89413a2faf564b01ae268c5daa6fa37d2f5c8
zdd <291856414@qq.com.com>
2019-11-13 21:28:21 +0800

    接口

---
+++ server/src/routers/goods.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const Router = express.Router();
+
+// 引入数据库操作方式
+const mongodb = require('../db/mongodb')
+const {formatData} = require('../utils')
+
+// 编写数据接口
+
+const colName = 'goodlist';
+
+// @查询所有商品
+Router.get('/',async (req,res)=>{
+  
+    // let {page=1,size=20,sort} = req.query;
+
+    // 根据分页和每页数量计算跳过的索引值
+    // let index = (page-1)*size
+  let {type}=  req.params
+    let data = await mongodb.find(colName,{"type" : `${type}`});
+    res.send(formatData({data}))
+})
+
+
+// @添加商品
+Router.post('/',async (req,res)=>{
+
+    let sql = `insert into goods(`
+    for(let key in req.body){
+        sql += `${key},`
+    }
+    sql = sql.replace(/,$/,') values(')
+    for(let key in req.body){
+        sql += `"${req.body[key]}",`
+    }
+    sql = sql.replace(/,$/,')');
+    console.log(sql);
+    let result = await query(sql)
+    res.send(result)
+})
+
+// @查询单个商品
+Router.get('/:id',(req,res)=>{
+    
+    let {id} = req.params;
+    // 查询数据：
+    let sql = `select * from goods where id=${id}`;
+
+
+    query(sql,function(data){
+        res.send(data)
+    });
+})
+module.exports = Router
